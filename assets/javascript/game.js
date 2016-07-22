16047ad9d757c741af995443c3298c130633ae0f
DaveSpeak <DaveSpeak@gmail.com>
2016-07-21 17:41:58 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,6 +23,7 @@
 				dashes+="-";
 			}
 			words[4].displayHead();
+			return;
 		}
 
 		}, 
@@ -54,7 +55,7 @@
 			}
 
 		}
-
+		return;
 		}
 
 		}, 
@@ -89,6 +90,7 @@
 			"</strong></h1><h1><strong>Current word:</strong></h1>"+"<h1><strong>"+words[3].spreadDisplay(dashes," ",true)+
 			"</strong></h1><h1><strong>Letters Already Guessed:</strong></h1>"+"<h1><strong>"+words[3].spreadDisplay(lettersGuessed,", ",false)+"</strong>";
 			gameDiv.appendChild(mainHead);
+			return;
 		}
 		}, 
 		{word:"galaxy", picture:"galaxy.jpg", winSound:"254037__jagadamba__stereo-space-sound.wav",
@@ -109,6 +111,7 @@
 			document.getElementById("bonusText").innerHTML = "You Win!!";
 			var win = new Audio('assets/sounds/'+words[i].winSound);
 			win.play();
+			return;
 		}
 		}, 
 		{word:"Jupiter", picture:"jupiter.jpg", winSound:"195745__mishicu__mod-pad2.wav",
@@ -118,127 +121,13 @@
 			document.getElementById("bonusText").innerHTML = "Sorry, you lose. The word was '"+words[i].word+"'";
 			var win = new Audio('assets/sounds/254278__jagadamba__burp-mechanical-robot-robotic.wav');
 			win.play();
+			return;
 		}
 
 		}, 
 		{word:"Venus", picture:"venus.jpg", winSound:"214072__wubitog__space-beam.wav"}, 
 		{word:"SpaceShuttle", picture:"spaceshuttle.jpg", winSound:"259285__philip-daniels__launch.wav"}
 		]
-	// var execution = {	
-	// 		startGame: function (){
-	// 			answerIndex = Math.floor(Math.random()*words.length);
-	// 			answer = words[answerIndex].word;
-	// 			guesses=maxGuesses;
-	// 			lettersGuessed="";
-	// 			dashes="";
-	// 			console.log(answer);
-	// 			for(var i = 0; i < answer.length; i++) {
-	// 				dashes+="-";
-	// 			}
-	// 			displayHead();
-	// 		}
-	// 	}
-
-
-
-		
-
-	// FUNCTIONS
-	// ==============================================================================
-	// function startGame(){
-	// 	answerIndex = Math.floor(Math.random()*words.length);
-	// 	answer = words[answerIndex].word;
-	// 	guesses=maxGuesses;
-	// 	lettersGuessed="";
-	// 	dashes="";
-	// 	console.log(answer);
-	// 	for(var i = 0; i < answer.length; i++) {
-	// 		dashes+="-";
-	// 	}
-	// 	displayHead();
-	// }
-
-
-	// function playGame() {
-	// 	document.onkeyup = function(event){
-	// 		if (dashes===answer){execution.startGame();}
-	// 		var event = event.keyCode;
-	// 		if (event == 116) {event = -1;}
-	// 		var letter = String.fromCharCode(event).toLowerCase();
-	// 		if (checkUsed(lettersGuessed,letter) && letter.match(/[a-z]/i)) {
-	// 			lettersGuessed+=letter;
-	// 			guesses--;
-	// 			for (i=0;i<answer.length;i++) {
-	// 				var test = answer.charAt(i);
-	// 				if (letter==test.toLowerCase()){
-	// 					dashes=setCharAt(dashes,i,test);
-	// 				}
-	// 			}
-	// 			displayHead();
-	// 			if (dashes === answer){
-	// 				wins++;
-	// 				winReplace(answerIndex);
-	// 			}else if (guesses==0){
-	// 				losses++;
-	// 				loseReplace(answerIndex);
-	// 				execution.startGame();
-	// 			}
-	// 		}
-
-	// 	}
-	// }
-
-	// function setCharAt(str,index,chr) {
- //    	if(index > str.length-1) return str;
- //    return str.substr(0,index) + chr + str.substr(index+1);
-	// }
-
-	// function spreadDisplay(str,char,flag){
-	// 	var returnstr="";
-	// 	for (i=0;i<str.length*2;i++){
-	// 		if (i/2 === Math.floor(i/2)){
-	// 			returnstr+=str.charAt(i/2);
-	// 		}else {
-	// 			if (flag || i!==(str.length*2)-1){
-	// 			returnstr+=char;
-	// 			}	
-	// 		}
-	// 	}
-	// 	return returnstr;
-	// }
-	// function displayHead(){
-	// 	mainHead.innerHTML = "<h1><strong>Wins: "+wins+
-	// 	"</strong></h1><h1><strong>Losses: "+losses+
-	// 	"</strong></h1><h1><strong>Guesses Remaining: "+guesses+
-	// 	"</strong></h1><h1><strong>Current word:</strong></h1>"+"<h1><strong>"+words[3].spreadDisplay(dashes," ",true)+
-	// 	"</strong></h1><h1><strong>Letters Already Guessed:</strong></h1>"+"<h1><strong>"+words[3].spreadDisplay(lettersGuessed,", ",false)+"</strong>";
-	// 	gameDiv.appendChild(mainHead);
-
-	// }
-	// function checkUsed(str,ltr){
-	// 	for (i=0;i<str.length;i++){
-	// 		if (ltr === str.charAt(i)){
-	// 			return false;
-	// 		}
-	// 	}
-	// 	return true;
-
-	// }
-	// function winReplace(i){
-	// 	var newImage = document.getElementById("bonusImage").src="assets/images/"+words[i].picture;
-	// 	var newAlt = document.getElementById("bonusImage").alt=words[i].word;
-	// 	document.getElementById("bonusText").innerHTML = "You Win!!";
-	// 	var win = new Audio('assets/sounds/'+words[i].winSound);
-	// 	win.play();
-	// }
-	// function loseReplace(i){
-	// 	var newImage = document.getElementById("bonusImage").src="assets/images/"+words[i].picture;
-	// 	var newAlt = document.getElementById("bonusImage").alt=words[i].word;
-	// 	document.getElementById("bonusText").innerHTML = "Sorry, you lose. The word was '"+words[i].word+"'";
-	// 	var win = new Audio('assets/sounds/254278__jagadamba__burp-mechanical-robot-robotic.wav');
-	// 	win.play();
-	// }
-
 
 	// MAIN PROCESS
 	// ==============================================================================
